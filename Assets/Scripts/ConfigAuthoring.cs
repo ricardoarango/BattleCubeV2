9679ecc40d0e8e8a61e5a24fab6c3e355b3cdfde
Alejandro Lopez <alejandro@unity3d.com>
2023-10-31 12:34:21 -0500

    Create entities and set position on IJobParallelFor

--- Assets/Scripts/ConfigAuthoring.cs
+++ Assets/Scripts/ConfigAuthoring.cs
@@ -5,26 +5,22 @@ using UnityEngine;
 
 public class ConfigAuthoringAuthoring : MonoBehaviour
 {   
-    public GameObject redTeam;
-    public GameObject blueTeam;
-    
+    public GameObject prefab;
+    public int count;
+    public int initialRadius;
+    public int team;
     // Create an entity that has the prefabs 
     private class ConfigAuthoringBaker : Baker<ConfigAuthoringAuthoring>
     {
         public override void Bake(ConfigAuthoringAuthoring authoring)
         {
-            var entity = GetEntity(TransformUsageFlags.None);
+            var entity = GetEntity(TransformUsageFlags.Renderable);
             AddComponent(entity, new Spawner
             {
-                // By default, each authoring GameObject turns into an Entity.
-                // Given a GameObject (or authoring component), GetEntity looks up the resulting Entity.
-                PrefabRedTeam  = GetEntity(authoring.redTeam, TransformUsageFlags.Dynamic),
-                PrefabBlueTeam = GetEntity(authoring.blueTeam, TransformUsageFlags.Dynamic),
-                
-                //https://docs.unity3d.com/Packages/com.unity.entities@1.0/manual/ecs-workflow-create-entities.html
-                SpawnPosition = authoring.transform.position,
-                NextSpawnTime = 0.0f,
-                SpawnRate = 1
+                Prefab  = GetEntity(authoring.prefab, TransformUsageFlags.Dynamic),
+                Count = authoring.count,
+                InitialRadius = authoring.initialRadius,
+                Team = authoring.team
             });
         }
     }
@@ -32,10 +28,10 @@ public class ConfigAuthoringAuthoring : MonoBehaviour
 
 public struct Spawner : IComponentData
 {
-    public Entity PrefabRedTeam;
-    public Entity PrefabBlueTeam;
-    public float3 SpawnPosition;
-    public float NextSpawnTime;
-    public float SpawnRate;
-    
+    public Entity Prefab;
+    public int Count;
+    public int InitialRadius;
+    public int Team;
+    //public Entity PrefabBlueTeam;
+    //public float3 SpawnPosition;
 }
--- Assets/Scripts/Systems/SpawnerSystem.cs
+++ Assets/Scripts/Systems/SpawnerSystem.cs
@@ -1,37 +1,117 @@
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = Unity.Mathematics.Random;
 
+[RequireMatchingQueriesForUpdate]
 [BurstCompile]
 public partial struct SpawnerSystem : ISystem
 {
-    public void OnCreate(ref SystemState state) { }
+    public void OnCreate(ref SystemState state)
+    {
+    }
     public void OnDestroy(ref SystemState state) { }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
-    {
-        // Queries for all Spawner components. Uses RefRW because this system wants
-        // to read from and write to the component. If the system only needed read-only
-        // access, it would use RefRO instead.
-        foreach (RefRW<Spawner> spawner in SystemAPI.Query<RefRW<Spawner>>())
+    { 
+        var localToWorldLookup = SystemAPI.GetComponentLookup<LocalToWorld>();
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+        var world = state.World.Unmanaged;
+        
+        foreach (var (spawner, spawnerLocalToWorld,entity) in
+                                                   SystemAPI.Query<RefRO<Spawner>, RefRO<LocalToWorld>>().WithEntityAccess())
+            
         {
-            ProcessSpawner(ref state, spawner);
+            var entities =
+                CollectionHelper.CreateNativeArray<Entity, RewindableAllocator>(spawner.ValueRO.Count, ref world.UpdateAllocator);
+            
+            state.EntityManager.Instantiate(spawner.ValueRO.Prefab, entities);
+            var team = spawner.ValueRO.Team;
+            var setLocalToWorldJob = new SetEntityLocalToWorld
+            {
+                LocalToWorldFromEntity = localToWorldLookup,
+                Entities = entities,
+                Center = spawnerLocalToWorld.ValueRO.Position,
+                Radius = spawner.ValueRO.InitialRadius
+            };
+
+            state.Dependency = setLocalToWorldJob.Schedule(spawner.ValueRO.Count, 64, state.Dependency);
+            state.Dependency.Complete();
+            ecb.DestroyEntity(entity);
+            
+            /*EntityCommandBuffer.ParallelWriter ecb = GetEntityCommandBuffer(ref state);
+           new SoliderSpawnJob
+           {
+               team = team,
+               count = 40,
+               Ecb = ecb
+           }.ScheduleParallel();*/
         }
+
+        ecb.Playback(state.EntityManager);
+        
+        
     }
-
-    private void ProcessSpawner(ref SystemState state, RefRW<Spawner> spawner)
+  
+    private EntityCommandBuffer.ParallelWriter GetEntityCommandBuffer(ref SystemState state)
     {
-        // If the next spawn time has passed.
-        if (spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
-        {
-            // Spawns a new entity and positions it at the spawner.
-            Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.PrefabRedTeam);
-            // LocalPosition.FromPosition returns a Transform initialized with the given position.
-            state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(spawner.ValueRO.SpawnPosition));
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        return ecb.AsParallelWriter();
+    }
+}
 
-            // Resets the next spawn time.
-            spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+[BurstCompile]
+struct SetEntityLocalToWorld : IJobParallelFor
+{
+    [NativeDisableContainerSafetyRestriction] [NativeDisableParallelForRestriction]
+    public ComponentLookup<LocalToWorld> LocalToWorldFromEntity;
+    public NativeArray<Entity> Entities;
+    public float3 Center;
+    public float Radius;
+
+    public void Execute(int i)
+    {
+        
+        var entity = Entities[i];
+        var random = new Random(((uint)(entity.Index + i + 1) * 0x9F6ABC1));
+        float2 randomPos = random.NextFloat2Direction();
+        Debug.Log(randomPos);
+        var pos = new float3(Center.x , 0, Center.z );
+        var localToWorld = new LocalToWorld
+        {
+            Value = float4x4.TRS(pos, quaternion.identity, new float3(1.0f, 1.0f, 1.0f))
+        };
+        LocalToWorldFromEntity[entity] = localToWorld;
+
+    }
+}
+    
+public partial struct SoliderSpawnJob :  IJobEntity
+{
+    public int team;
+    public EntityCommandBuffer.ParallelWriter Ecb;
+    public int count;
+    public void Execute([EntityIndexInQuery] int index, in Spawner spawner)
+    {
+        var teamLocation = team == 0? 1 : -1;
+        float offset = 2f;
+        for (int x = 0; x < count; x++)
+        {
+            for (int z = 0; z < count; z++)
+            {
+                var pos = new float3(x * offset , 0, z * teamLocation *offset );
+                var newEntity = Ecb.Instantiate(index, spawner.Prefab);
+                Ecb.SetComponent(index, newEntity, LocalTransform.FromPosition(pos));
+            }
         }
     }
 }
+    
